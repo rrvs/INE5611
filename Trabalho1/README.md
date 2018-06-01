c013f2e0f5828f7d40214f772e93f78583bc9d99
Roberto Rivelino <robertorivelino3@gmail.com>
2018-06-01 17:19:21 -0300

    Pequenas alteraçõs no README e nas descrições de cada método

--- Trabalho1/README.md
+++ Trabalho1/README.md
@@ -1,2 +1,17 @@
-# Trabalho 1 - Sistemas Operacionais
-  GOMOKU
+# Trabalho 1 (T1)
+
+# Descrição do Problema
+
+Gomoku é um jogo de tabuleiro para dois jogadores. Os jogadores alternam em colocar uma pedra de sua cor (branco e preto) em um quadrado do tabuleiro que tem 8 colunas e 8 linhas, e quem tiver cinco (5) pedras consecutivas na horizontal, vertical ou diagonal ganha o jogo. Gomoku-N é uma versão estendida de Gomoku, alterando o número de pedras consecutivas vencedoras para N e o tamanho do tabuleiro é 2 * N por 2 * N. Por exemplo, se N = 6, então o tamanho do tabuleiro é 12 x 12 e o jogador que tem 6 pedras consecutivas na horizontal, vertical ou diagonal ganha o jogo. Quando o tabuleiro está cheio e não há vencedor, o jogo termina empatado. 
+
+# Objetivo do trabalho
+
+O objetivo deste trabalho é implementar um programa que permita que dois jogadores disputem uma partida de Gomoku-N  por meio do computador.
+
+Quando um jogador coloca uma pedra no tabuleiro, o programa deve verificar se o jogador venceu o jogo. Para tal, o programa determina se a nova pedra conectou N pedras juntas horizontalmente, verticalmente ou diagonalmente. As verificações das três direções são independentes umas das outras. Pthreads devem realizar a verificação de cada uma das três direções.
+
+O tabuleiro é modelado como uma matriz de 2N * 2N, onde N é um valor fornecido quando do início da partida e a célula superior esquerda é a de posição (0, 0) e a inferior direita é (2N-1, 2N-1). Após definido o valor de N, o programa determina aleatoriamente o jogador que inicia a partida. O  jogador faz sua jogada inserindo as coordenadas da célula em que deseja colocar a pedra e o formato de entrada deve ser a linha, coluna. Por exemplo, se o jogador quiser colocar a pedra na célula da linha 0 e coluna 5, o jogador deve inserir 0 5. Após cada jogada, é mostrado o estado do tabuleiro atualizado (um jogador pode ser representado por 'X' e o outro por 'O'). No final do jogo, o programa informa o vencedor ou empate.
+
+# Método
+
+A implementação deve ser realizada na linguagem C. A versão paralela deve ser implementada para ambiente Unix usando pthreads.
--- Trabalho1/trabalhoSO.c
+++ Trabalho1/trabalhoSO.c
@@ -7,7 +7,7 @@ int vitoria = 0;
 int empate = 0;
 int tamanho;
 
-
+// chega se há vencedor na horizontal
 void *vencedor_horizontal(void *arg_tabuleiro) {
     int **tabuleiro = (int **)arg_tabuleiro;
     for (int linha = 0; linha < tamanho; linha++) {
@@ -32,6 +32,7 @@ void *vencedor_horizontal(void *arg_tabuleiro) {
     return NULL;
 }
 
+//checa se há vencedor na vertical
 void *vencedor_vertical(void *arg_tabuleiro) {
     int **tabuleiro = (int **)arg_tabuleiro;
     for (int linha = 0; linha < tamanho; linha++) {
@@ -49,24 +50,12 @@ void *vencedor_vertical(void *arg_tabuleiro) {
     return NULL;
 }
 
+//checa se há vencedor na diagonal
 void *test_venceu_diagonal(void *arg_tabuleiro) {
-    int **tabuleiro = (int **)arg_tabuleiro;
-    for (int i = 0; i < tamanho; i++) {
-        int index_vencer = 0;
-        for (int j = 0; j < tamanho; j++) {
-            if (tabuleiro[i][j] == jogador_vez) {
-                index_vencer++;
-            }
-        }
-        if (index_vencer >= tamanho/2) {
-            printf("\nJogador %d é o vencedor!\n", jogador_vez);
-            vitoria = 1;
-        }
-    }
-    return NULL;
 }
 
 
+// método main para a execução do Gomoku
 int main() {
 
     int numero_pecas;
@@ -74,12 +63,12 @@ int main() {
     scanf("%d", &numero_pecas);
     tamanho = numero_pecas*2;
 
-    //cria uma matriz com largura = tamanho e altura = tamanho
+    //cria tabuleiro 2*N por 2*N
     int **tabuleiro = (int **)malloc(tamanho * sizeof(int *));
     for (int i=0; i<tamanho; i++)
          tabuleiro[i] = (int *)malloc(tamanho * sizeof(int));
 
-    //preenche o tabuleiro com 0
+    //preenche tabuleiro com o inteiro 0
     for (int i = 0; i < tamanho; i++) {
         for (int j = 0; j < tamanho; j++) {
             tabuleiro[i][j] = 0;
@@ -87,7 +76,7 @@ int main() {
     }
 
 
-    //imprime o tabuleiro
+    //imprime tabuleiro
     printf("\n  |");
     for (int i = 0; i < tamanho; i++) {
         printf("%d  ", i);
@@ -104,25 +93,23 @@ int main() {
         }
         printf("\n");
     }
-    //terminou de imprimir
 
-
-    //gera aleatoriamente um primeiro jogador
+    //gera jogador randomico
     srand(time(NULL));
     jogador_vez = (rand() % 2) + 1;
 
-    //fica num loop até ou alguem ganhar ou empatar
+    //executa até haver um vencedor
     while(vitoria == 0 && empate == 0) {
 
         printf("\nVez do jogador %d\n\n", jogador_vez);
 
-        //usuário joga
+        //jogadas
         int sucesso = 0;
         do {
             printf("Digite a linha e a coluna:\n");
             int linha, coluna;
             scanf("%d %d", &linha, &coluna);
-	    //checa se os valores inseridos estão dentro do tabuleiro e também se é uma casa vazia
+	    //checa coordenada existe e não há nenhuma pedra
             if ((linha < tamanho && coluna < tamanho) && tabuleiro[linha][coluna] == 0) {
                 tabuleiro[linha][coluna] = jogador_vez;
                 sucesso = 1;
@@ -132,7 +119,7 @@ int main() {
         } while (sucesso == 0);
 
 
-        //imprime o tabuleiro
+        //imprime o tabuleiro ao final de cada jogada
         printf("\n  |");
         for (int i = 0; i < tamanho; i++) {
             printf("%d  ", i);
@@ -149,25 +136,24 @@ int main() {
             }
             printf("\n");
         }
-        //terminou de imprimir
 
 
-        //identificadores das threads
+        //POSIX threads
         pthread_t checa_horiz;
         pthread_t checa_vertical;
         pthread_t checa_diagonal;
 
-        //criação de threads
+        //criação
         pthread_create(&checa_horiz, NULL, vencedor_horizontal, tabuleiro);
         pthread_create(&checa_vertical, NULL, vencedor_vertical, tabuleiro);
         pthread_create(&checa_diagonal, NULL, test_venceu_diagonal, tabuleiro);
 
-        //join das threads
+        //execução em join
         pthread_join(checa_horiz, NULL);
         pthread_join(checa_vertical, NULL);
         pthread_join(checa_diagonal, NULL);
 
-        //avança pro próximo jogador
+        //próximo jogador
         jogador_vez = (jogador_vez % 2) + 1;
     }
 
